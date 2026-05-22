db72978b51d5b4ec37b99564d0661e368de1a5a1
Badgiro <mulbashev98@gmail.com>
2026-05-22 15:05:02 +0300

    fixed scroll

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const openBtn = document.querySelector('.open-btn')
 const closeBtn = document.querySelector('#closeBtn')
 const modalOverlay = document.querySelector('#modalOverlay')
-
 openBtn.onclick = () => {
   modalOverlay.classList.add('active')
+  // document.body.classList.add('overflowHidden')
+  document.body.style.overflow = 'hidden'
 }
 closeBtn.onclick = () => {
   modalOverlay.classList.remove('active')
+  // document.body.classList.remove('overflowHidden')
+  document.body.style.overflow = 'auto'
 }
 modalOverlay.onclick = (e) => {
   if (e.target === modalOverlay) {
     modalOverlay.classList.remove('active')
+    // document.body.classList.remove('overflowHidden')
+    document.body.style.overflow = 'auto'
   }
 }
